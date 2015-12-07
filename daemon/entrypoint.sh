4ec67cdc894f2a9360afc285edfb213c77ab6afd
Emmanuel <eleroy@msn.com>
2015-12-07 14:40:24 -0800

    typo

--- daemon/entrypoint.sh
+++ daemon/entrypoint.sh
@@ -249,7 +249,7 @@ function osd_directory {
       ceph ${CEPH_OPTS} --name=osd.${OSD_ID} --keyring=/var/lib/ceph/osd/${CLUSTER}-${OSD_ID}/keyring osd crush create-or-move -- ${OSD_ID} ${OSD_WEIGHT} ${CRUSH_LOCATION}
     fi
 
-    eco $(date '+%Y %b %d %H:%M:%S') "build the service"
+    echo $(date '+%Y %b %d %H:%M:%S') "build the service"
     mkdir -p /etc/service/${CLUSTER}-${OSD_ID}
     cat >/etc/service/${CLUSTER}-${OSD_ID}/run <<EOF
 #!/bin/bash
