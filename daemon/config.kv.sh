b48c11e3d8b8605f0dae82062b3332f471a1b8b9
Emmanuel <eleroy@msn.com>
2015-12-21 21:57:36 -0800

    handle error when key exists

--- daemon/config.kv.sh
+++ daemon/config.kv.sh
@@ -43,12 +43,12 @@ function get_mon_config {
   # making sure the root dirs are present for the confd to work with etcd
   if [[ "$KV_TYPE" == "etcd" ]]; then
     echo "creating default directories"
-    etcdctl -C ${KV_IP}:${KV_PORT} mkdir ${CLUSTER_PATH}/auth #> /dev/null 2>&1
-    etcdctl -C ${KV_IP}:${KV_PORT} mkdir ${CLUSTER_PATH}/global #> /dev/null 2>&1
-    etcdctl -C ${KV_IP}:${KV_PORT} mkdir ${CLUSTER_PATH}/mon #> /dev/null 2>&1
-    etcdctl -C ${KV_IP}:${KV_PORT} mkdir ${CLUSTER_PATH}/mds #> /dev/null 2>&1
-    etcdctl -C ${KV_IP}:${KV_PORT} mkdir ${CLUSTER_PATH}/osd #> /dev/null 2>&1
-    etcdctl -C ${KV_IP}:${KV_PORT} mkdir ${CLUSTER_PATH}/client #> /dev/null 2>&1
+    etcdctl -C ${KV_IP}:${KV_PORT} mkdir ${CLUSTER_PATH}/auth > /dev/null 2>&1 || echo "key already exists"
+    etcdctl -C ${KV_IP}:${KV_PORT} mkdir ${CLUSTER_PATH}/global > /dev/null 2>&1  || echo "key already exists"
+    etcdctl -C ${KV_IP}:${KV_PORT} mkdir ${CLUSTER_PATH}/mon > /dev/null 2>&1  || echo "key already exists"
+    etcdctl -C ${KV_IP}:${KV_PORT} mkdir ${CLUSTER_PATH}/mds > /dev/null 2>&1  || echo "key already exists"
+    etcdctl -C ${KV_IP}:${KV_PORT} mkdir ${CLUSTER_PATH}/osd > /dev/null 2>&1   || echo "key already exists"
+    etcdctl -C ${KV_IP}:${KV_PORT} mkdir ${CLUSTER_PATH}/client > /dev/null 2>&1  || echo "key already exists"
   fi
 
   echo "Adding Mon Host - ${MON_NAME}"
